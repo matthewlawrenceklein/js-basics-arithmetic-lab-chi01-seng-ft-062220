6da18f4d034c7bb274e8a1ccb2d3709667f7a9cc
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-08-02 10:33:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+var var1 = 31 ;
+var var2 = 2 ;  
+const multiply = var1 * var2 ; 
+
+const random = Math.floor(Math.random() * 10) + 1 ; 
+
+const mod = 10 % 6 ; 
+
+const max = Math.max(2,4,6,20)
